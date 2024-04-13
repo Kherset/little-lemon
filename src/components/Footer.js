5e11fb0b4655eb47a516d56e7f7257661122dec0
jeremy twardowski <kherset@protonmail.com>
2024-04-13 15:11:56 +0200

    Semantic + meta datas

---
+++ src/components/Footer.js
@@ -0,0 +1,62 @@
+import logo from "../img/little-lemon-footer.png";
+
+function Footer() {
+  return (
+    <footer>
+      <img src={logo} alt="Logo little Lemon" />
+      <div>
+        <h3>Important Links</h3>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Menu</a>
+          </li>
+          <li>
+            <a href="/">Reservations</a>
+          </li>
+          <li>
+            <a href="/">Order Online</a>
+          </li>
+          <li>
+            <a href="/">Login</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3>Contact</h3>
+        <ul>
+          <li>
+            Address: <br /> 123 Town Street, Chicago
+          </li>
+          <li>
+            Phone: <br /> +00 123 456 789
+          </li>
+          <li>
+            Email: <br /> little@lemon.com
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3>Social Media Links</h3>
+        <ul>
+          <li>
+            <a href="/">Facebook</a>
+          </li>
+          <li>
+            <a href="/">Instagram</a>
+          </li>
+          <li>
+            <a href="/">Twitter</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import logo from "../img/little-lemon.png";
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="Logo little Lemon" />
+    </header>
+  );
+}
+
+export default Header;
